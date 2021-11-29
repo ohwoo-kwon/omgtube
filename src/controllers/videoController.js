16779812151b5b1a399686d16d22b6c537a75933
ohwoo <kwonow1@gmail.com>
2021-11-29 21:52:21 +0900

    5. templates

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,14 +1,34 @@
 export const trending = (req, res) => {
-  res.send("Homepage Videos");
+  const videos = [
+    {
+      title: "First Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes agoe",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Second Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes agoe",
+      views: 59,
+      id: 2,
+    },
+    {
+      title: "Third Video",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes agoe",
+      views: 59,
+      id: 3,
+    },
+  ];
+  res.render("home", { pageTitle: "Home", videos });
 };
-
-export const see = (req, res) => {
-  res.send(`Watch Video #${req.params.id}`);
-};
-
-export const edit = (req, res) => {
-  res.send("Edit Video");
-};
+export const see = (req, res) => res.render("watch", { pageTitle: "Watch" });
+export const edit = (req, res) => res.render("edit", { pageTitle: "Edit" });
 
 export const search = (req, res) => {
   res.send("Search");
