74f57906f1900d59d23c0e294f6c2f8e66d62140
ohwoo <kwonow1@gmail.com>
2021-11-29 12:55:24 +0900

    introduction to express

--- src/server.js
+++ src/server.js
@@ -1,8 +1,17 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const logger = morgan("dev");
+
+const handleHome = (req, res, next) => {
+  return res.end();
+};
+
+app.use(logger);
+app.get("/", logger, handleHome);
 
 const handelListening = () => {
   console.log(`Server listening on http://localhost:${PORT}`);
