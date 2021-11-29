c459190f6a5340d91b7f9b1685fdfddb86f99f76
ohwoo <kwonow1@gmail.com>
2021-11-29 14:57:34 +0900

    Router Setting

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -9,3 +9,15 @@ export const edit = (req, res) => {
 export const remove = (req, res) => {
   res.send("Remove User");
 };
+
+export const login = (req, res) => {
+  res.send("login");
+};
+
+export const logout = (req, res) => {
+  res.send("logout");
+};
+
+export const see = (req, res) => {
+  res.send("see");
+};
