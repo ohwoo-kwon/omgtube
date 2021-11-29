64c7c2d6781e42913721112165cfd9d2116504f3
ohwoo <kwonow1@gmail.com>
2021-11-29 14:30:11 +0900

    Router

---
+++ src/controllers/videoController.js
@@ -0,0 +1,11 @@
+export const trending = (req, res) => {
+  res.send("Homepage Videos");
+};
+
+export const watch = (req, res) => {
+  res.send("Watch Video");
+};
+
+export const edit = (req, res) => {
+  res.send("Edit Video");
+};
--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const handleHome = (req, res, next) => {
-  return res.end();
-};
-
 app.use(logger);
-app.get("/", logger, handleHome);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handelListening = () => {
   console.log(`Server listening on http://localhost:${PORT}`);
