09366203720e52779c21d671273d5d8a751276b2
ohwoo <kwonow1@gmail.com>
2021-12-01 16:26:42 +0900

    upload video

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -1,19 +1,17 @@
 import express from "express";
 import {
   watch,
-  upload,
-  deleteVideo,
   getEdit,
   postEdit,
+  getUpload,
+  postUpload,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
 
-videoRouter.get("/upload", upload);
 videoRouter.get("/:id(\\d+)", watch);
 videoRouter.route("/:id(\\d+)/edit").get(getEdit).post(postEdit);
-// videoRouter.get("/:id(\\d+)/edit", getEdit);
-// videoRouter.post("/:id(\\d+)/edit", postEdit);
-videoRouter.get("/:id(\\d+)/delete", deleteVideo);
+videoRouter.get("/upload", getUpload);
+videoRouter.post("/upload", postUpload);
 
 export default videoRouter;
