77d3249476d989b57997e6962f7e3a1bfa9faa1c
ohwoo <kwonow1@gmail.com>
2021-12-17 09:26:49 +0900

    8.1 Protector and Public Middlewares

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { all } from "express/lib/application";
 import {
   getEdit,
   postEdit,
@@ -7,13 +8,14 @@ import {
   startGithubLogin,
   finishGithubLogin,
 } from "../controllers/userController";
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
 
 const userRouter = express.Router();
 
-userRouter.get("/logout", logout);
-userRouter.route("/edit").get(getEdit).post(postEdit);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
+userRouter.get("/logout", protectorMiddleware, logout);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
