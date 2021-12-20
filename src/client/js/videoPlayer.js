f07f7c989d8f4a9aeb883178ba6d9cd81fd460bf
ohwoo <kwonow1@gmail.com>
2021-12-20 20:23:28 +0900

    11.1 Play Pause

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,22 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.querySelector("#play");
+const muteBtn = document.querySelector("#mute");
+const time = document.querySelector("#time");
+const volume = document.querySelector("#volume");
+
+const handlePlayClick = (e) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+const handleMute = (e) => {};
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
