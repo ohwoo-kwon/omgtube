dcec25a34610357b6ae58ba37ef9c18a7912bd84
ohwoo <kwonow1@gmail.com>
2021-12-23 11:55:13 +0900

    13 Record and Download

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,11 +1,50 @@
 const startBtn = document.querySelector("#startBtn");
+const video = document.querySelector("#preview");
 
-const handleStart = async () => {
-  const stream = await navigator.mediaDevices.getUserMedia({
+let stream;
+let recorder;
+let videoFile;
+
+const handelDownload = () => {
+  const a = document.createElement("a");
+  a.href = videoFile;
+  a.download = "MyRecording.mp4";
+  document.body.appendChild(a);
+  a.click();
+  document.body.removeChild(a);
+};
+
+const handleStop = () => {
+  startBtn.innerText = "Download Recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handelDownload);
+  recorder.stop();
+};
+
+const handleStart = () => {
+  startBtn.innerText = "Stop Recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click", handleStop);
+  recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (event) => {
+    videoFile = URL.createObjectURL(event.data);
+    video.srcObject = null;
+    video.src = videoFile;
+    video.loop = true;
+    video.play();
+  };
+  recorder.start();
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
     audio: true,
     video: true,
   });
-  console.log(stream);
+  video.srcObject = stream;
+  video.play();
 };
+
+init();
 
 startBtn.addEventListener("click", handleStart);
