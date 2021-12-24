7a37bc8fdb2fa4d977295671ccfbc0ed8d2a7422
ohwoo <kwonow1@gmail.com>
2021-12-24 14:02:14 +0900

    16.7 Realtime Comments

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,6 +1,19 @@
 const videoContainer = document.querySelector("#videoContainer");
 const form = document.querySelector("#commentForm");
 
+const addComment = (text) => {
+  const videoComments = document.querySelector(".video__comments ul");
+  const newComment = document.createElement("li");
+  const icon = document.createElement("i");
+  const span = document.createElement("span");
+  newComment.className = "video__comment";
+  icon.className = "fas fa-comment";
+  span.innerText = ` ${text}`;
+  newComment.appendChild(icon);
+  newComment.appendChild(span);
+  videoComments.prepend(newComment);
+};
+
 const handleSubmit = async (event) => {
   event.preventDefault();
   const textarea = form.querySelector("textarea");
@@ -9,13 +22,15 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  await fetch(`/api/videos/${videoId}/comment`, {
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text }),
   });
   textarea.value = "";
-  window.location.reload();
+  if (status === 201) {
+    addComment(text);
+  }
 };
 
 if (form) {
