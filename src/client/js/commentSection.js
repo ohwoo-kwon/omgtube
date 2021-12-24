2709bff8598337622ad0758a61decd6c9cd7594e
ohwoo <kwonow1@gmail.com>
2021-12-24 14:28:17 +0900

    16.8 Comment Id

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,16 +1,20 @@
 const videoContainer = document.querySelector("#videoContainer");
 const form = document.querySelector("#commentForm");
 
-const addComment = (text) => {
+const addComment = (text, id) => {
   const videoComments = document.querySelector(".video__comments ul");
   const newComment = document.createElement("li");
   const icon = document.createElement("i");
   const span = document.createElement("span");
+  const span2 = document.createElement("span");
   newComment.className = "video__comment";
   icon.className = "fas fa-comment";
   span.innerText = ` ${text}`;
+  span2.innerText = " ❌";
   newComment.appendChild(icon);
   newComment.appendChild(span);
+  newComment.appendChild(span2);
+  newComment.dataset.id = id;
   videoComments.prepend(newComment);
 };
 
@@ -22,14 +26,15 @@ const handleSubmit = async (event) => {
   if (text === "") {
     return;
   }
-  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+  const response = await fetch(`/api/videos/${videoId}/comment`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ text }),
   });
-  textarea.value = "";
-  if (status === 201) {
-    addComment(text);
+  if (response.status === 201) {
+    const { newCommentId } = await response.json();
+    addComment(text, newCommentId);
+    textarea.value = "";
   }
 };
 
