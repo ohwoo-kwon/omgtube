58811eeb973adb9702d215991dfbf420269f61d4
ohwoo <kwonow1@gmail.com>
2021-12-26 14:56:56 +0900

    PORT env variable

--- src/init.js
+++ src/init.js
@@ -6,7 +6,7 @@ import "./models/User.js";
 import "./models/Comment.js";
 import app from "./server.js";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handelListening = () => {
   console.log(`Server listening on http://localhost:${PORT}`);
